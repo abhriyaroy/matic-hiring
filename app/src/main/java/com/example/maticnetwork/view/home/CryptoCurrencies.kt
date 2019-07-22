0be6acf484cbead13c0ac1b4c3e22fc7552c9f69
Abhriya <roy.abhriya@gmail.com>
2019-07-22 07:55:02 +0530

    add names and abbreviations

---
+++ app/src/main/java/com/example/maticnetwork/view/home/CryptoCurrencies.kt
@@ -0,0 +1,74 @@
+package com.example.maticnetwork.view.home
+
+import com.example.maticnetwork.R
+
+object CryptoCurrencies {
+
+  fun getImagesList() = listOf(
+    R.drawable.ic_abt,
+    R.drawable.ic_act,
+    R.drawable.ic_amp,
+    R.drawable.ic_brd,
+    R.drawable.ic_crpt,
+    R.drawable.ic_edo,
+    R.drawable.ic_eng,
+    R.drawable.ic_evx,
+    R.drawable.ic_fct,
+    R.drawable.ic_generic,
+    R.drawable.ic_gxs,
+    R.drawable.ic_html,
+    R.drawable.ic_hush,
+    R.drawable.ic_ignis,
+    R.drawable.ic_kcs,
+    R.drawable.ic_krb,
+    R.drawable.ic_mds,
+    R.drawable.ic_mft,
+    R.drawable.ic_payx,
+    R.drawable.ic_pungo
+  )
+
+  fun getHeadingsList() = listOf(
+    "ArcBlock",
+    "Achain",
+    "AMP",
+    "Bread",
+    "Eidoo",
+    "Enigma",
+    "Everex",
+    "Factom",
+    "Generic",
+    "GXChain",
+    "HTML",
+    "Hush",
+    "Ignis",
+    "KuCoin Shares",
+    "Karbo",
+    "MediShares",
+    "Mainframe",
+    "Paypex",
+    "Pungo"
+  )
+
+  fun getAbbreviations() = listOf(
+    "ABT",
+    "ACT",
+    "BRD",
+    "CRPT",
+    "EDO",
+    "ENG",
+    "EVX",
+    "FCT",
+    "GENERIC",
+    "GXS",
+    "HTML",
+    "HUSH",
+    "IGNIS",
+    "KCS",
+    "KRB",
+    "MDS",
+    "MFT",
+    "PAYX",
+    "PUNGO"
+  )
+
+}
