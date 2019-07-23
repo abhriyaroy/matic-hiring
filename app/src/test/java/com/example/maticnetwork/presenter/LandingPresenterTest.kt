d1972184b65ca1cff24ef157f493432138f464f8
Abhriya <roy.abhriya@gmail.com>
2019-07-23 10:22:04 +0530

    Test landing presenter

---
+++ app/src/test/java/com/example/maticnetwork/presenter/LandingPresenterTest.kt
@@ -0,0 +1,47 @@
+package com.example.maticnetwork.presenter
+
+import com.example.maticnetwork.presenter.landing.LandingContract.LandingPresenter
+import com.example.maticnetwork.presenter.landing.LandingContract.LandingView
+import com.example.maticnetwork.presenter.landing.LandingPresenterImpl
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.verifyNoMoreInteractions
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class LandingPresenterTest {
+
+  @Mock
+  private lateinit var landingViewMock: LandingView
+  private val landingPresenter: LandingPresenter = LandingPresenterImpl()
+
+  @Before
+  fun setup() {
+    landingPresenter.attachView(landingViewMock)
+  }
+
+  @Test
+  fun `should redirect to new user account page`() {
+    landingPresenter.handleNewAccountClick()
+
+    verify(landingViewMock).redirectToNewAccount()
+  }
+
+  @Test
+  fun `should redirect to sign in page`() {
+    landingPresenter.handleSignInClick()
+
+    verify(landingViewMock).redirectToSignIn()
+  }
+
+  @After
+  fun tearDown() {
+    landingPresenter.detachView()
+    verifyNoMoreInteractions(landingViewMock)
+  }
+
+}
